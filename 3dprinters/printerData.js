7a4c0abb947d9e1dbc17a771b0c5bb3f7ba8b236
niklas <niklaspollonen@gmail.com>
2019-01-07 14:28:32 +0200

    new version of 3dPrinters site

---
+++ 3dprinters/printerData.js
@@ -0,0 +1,59 @@
+var printerData = [
+  {
+    'name': 'Mini: nr 1',
+    "outOfService": 0,
+    'isOn' : 0,
+    'src' : "images/mini.png",
+    'link' : 'http://193.167.4.125',
+
+  },
+  {
+    'name': 'Mini: nr 2',
+    "outOfService": 0,
+    'isOn' : 0,
+    'src' : 'images/mini.png',
+    'link' : 'http://193.167.4.119',
+  },
+  {
+    'name': 'Mini: nr 3',
+    "outOfService": 0,
+    'isOn' : 0,
+    'src' : 'images/mini.png',
+    'link' : 'http://193.167.4.120',
+  },
+  {
+    'name': 'Mini: nr 4',
+    "outOfService": 0,
+    'isOn' : 0,
+    'src' : 'images/mini.png',
+    'link' : 'http://193.167.4.123',
+  },
+  // {
+  //   'name': 'Mini: nr 5',
+  //   "outOfService": 1,
+  //   'isOn' : 0,
+  //   'src' : 'images/mini.png',
+  //   'link' : '',
+  // },
+  // {
+  //   'name': 'Taz 6: nr 1',
+  //   "outOfService": 1,
+  //   'isOn' : 0,
+  //   'src' : 'images/taz6.png',
+  //   'link' : '',
+  // },
+  // {
+  //   'name': 'Taz 6: nr 1',
+  //   "outOfService": 1,
+  //   'isOn' : 0,
+  //   'src' : 'images/taz6.png',
+  //   'link' : '',
+  // },
+  // {
+  //   'name': 'Ultimaker',
+  //   "outOfService": 1,
+  //   'isOn' : 0,
+  //   'src' : 'images/ultimaker.png',
+  //   'link' : '',
+  // }
+]
